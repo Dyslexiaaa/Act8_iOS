bacd310ea250c192d302a84100b77b1668088c81
Dyslexiaaa <damsalgon@gmail.com>
2024-04-03 19:52:39 -0600

    Actividad8

---
+++ Lab - About me/ACT8IOS/ACT8IOS/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ACT8IOS
+//
+//  Created by Alumno on 01/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+}
+    
+    
+    
+    
+    
+}
+
